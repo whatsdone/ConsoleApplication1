50e9a1b36137f3cf15e46f2f551d558abf437afb
Hans Lundström <hassesmail@telia.com>
2019-09-15 19:11:31 +0200

    Added some extra text in main.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World! and the rest of universe\n"; 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
